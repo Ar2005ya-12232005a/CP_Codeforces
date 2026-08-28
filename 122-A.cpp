c7d30fba560043a2d61a9159e91d2a36a727fc53
Arya Sankar <aryasankar16@gmail.com>
2026-08-28 20:43:19 +0530

    3 prob

---
+++ 122-A.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool lucky(int n) {
+    while (n > 0) {
+        int digit = n % 10;
+
+        if (digit != 4 && digit != 7)
+            return false;
+
+        n /= 10;
+    }
+
+    return true;
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    for (int i = 1; i <= n; i++) {
+        if (n % i == 0 && lucky(i)) {
+            cout << "YES" << endl;
+            return 0;
+        }
+    }
+
+    cout << "NO" << endl;
+
+    return 0;
+}
