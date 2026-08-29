87d3db2c817a6ef835a9a4ee00c702f3b8c0eaa0
Arya Sankar <aryasankar16@gmail.com>
2026-08-29 20:09:41 +0530

    236-A

---
+++ 236-A.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+
+    set<char> uniqueChars;
+
+    for (char c : s) {
+        uniqueChars.insert(c);
+    }
+
+    if (uniqueChars.size() % 2 == 0)
+        cout << "CHAT WITH HER!" << endl;
+    else
+        cout << "IGNORE HIM!" << endl;
+
+    return 0;
+}
