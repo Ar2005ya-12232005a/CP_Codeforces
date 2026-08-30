d34bdfcae8e1be0bcc8ec9ba54dcc5eb399d5d0c
Arya Sankar <aryasankar16@gmail.com>
2026-08-30 20:14:57 +0530

    2 questions

---
+++ 734-A.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    string s;
+    cin >> s;
+
+    int A = 0, D = 0;
+
+    for (char c : s) {
+        if (c == 'A')
+            A++;
+        else
+            D++;
+    }
+
+    if (A > D)
+        cout << "Anton" << endl;
+    else if (D > A)
+        cout << "Danik" << endl;
+    else
+        cout << "Friendship" << endl;
+
+    return 0;
+}
